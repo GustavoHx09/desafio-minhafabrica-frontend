c51d72b4a5d3d8fd426a30228c5790268d48d2b8
GustavoHx09 <gustavo.henrique091104@gmail.com>
2026-04-15 18:37:51 -0300

    Fit: corrigido caminho da api, trocado da local para remota!

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1/", // sua API
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`, // api
 });
 
 // envia o token em toda requisição
