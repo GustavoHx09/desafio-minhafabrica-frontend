b4eff7a87f8a1c425bff816cec059ddf9d0b21fa
GustavoHx09 <gustavo.henrique091104@gmail.com>
2026-04-18 12:41:42 -0300

    Feat: adicionado na tela de login a fase 'Autenticando'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,18 @@ import { api } from "@/services/api";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-    const [messageError, setMessagerror] = useState("");
+    const [messageError, setMessageError] = useState("");
     const [email, setEmail] = useState("");
     const [pass, setPass] = useState("");
+    const [loading, setLoading] = useState(false);
+
     const router = useRouter();
 
     async function handleLogin(e: any) {
         e.preventDefault();
+
+        setLoading(true); // começa autenticação
+        setMessageError("");
 
         try {
             const response = await api.post("/auth/login", {
@@ -24,22 +29,23 @@ export default function LoginPage() {
             router.push("/admin/dashboard");
         } catch (err: any) {
             if (err.response?.data?.message) {
-                setMessagerror(err.response.data.message);
+                setMessageError(err.response.data.message);
             } else {
-                setMessagerror("Erro no login");
+                setMessageError("Erro no login");
             }
+        } finally {
+            setLoading(false); // termina a autencitação
         }
     }
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <form onSubmit={handleLogin} className="p-6 bg-white rounded shadow">
+            <form onSubmit={handleLogin} className="p-6 bg-white rounded shadow w-80">
                 <h1 className="text-xl mb-4">Login</h1>
 
                 {messageError && (
                     <p className="text-red-500 mb-2">{messageError}</p>
-                )
-                }
+                )}
 
                 <input
                     className="border p-2 w-full mb-2"
@@ -54,8 +60,11 @@ export default function LoginPage() {
                     onChange={(e) => setPass(e.target.value)}
                 />
 
-                <button className="bg-blue-500 text-white p-2 w-full">
-                    Entrar
+                <button
+                    disabled={loading}
+                    className="bg-blue-500 text-white p-2 w-full disabled:opacity-50"
+                >
+                    {loading && <p className="text-sm text-gray-500 mb-2">Autenticando...</p>}
                 </button>
             </form>
         </div>
